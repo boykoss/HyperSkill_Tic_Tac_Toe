4ccb956ce32aa5679f792ac77a95d1a1a775e1a9
My Name <ccrpted@gmail.com>
2023-02-21 10:51:44 +0300

    Stage 5/5: Fight! In this stage, you should write a program that:
    Prints an empty grid at the beginning of the game.
    Creates a game loop where the program asks the user to enter the cell coordinates, analyzes the move for correctness and shows a grid with the changes if everything is okay.
    Ends the game when someone wins or there is a draw.

--- Simple Tic-Tac-Toe (Python)/task/tictactoe.py
+++ Simple Tic-Tac-Toe (Python)/task/tictactoe.py
@@ -1,8 +1,6 @@
-data = input()
-
-
+data = "         "
 field = [i for i in data]
-
+shot = "X"
 updated_field = [
         field[0:3],
         field[3:6],
@@ -14,72 +12,82 @@ print("| " + " ".join(updated_field[1]) + " |")
 print("| " + " ".join(updated_field[2]) + " |")
 print("---------")
 
-while True:
+def check_condition(f, c):
+        board = [item for group in f for item in group]
+        xxx_gorizontal = (board[0] == board[1] == board[2] and board[0] == "X") or (
+                        board[3] == board[4] == board[5] and board[3] == "X") or (
+                                         board[6] == board[7] == board[8] and board[6] == "X")
+        xxx_diagonal = (board[0] == board[4] == board[8] and board[0] == "X") or (
+                        board[2] == board[4] == board[6] and board[2] == "X")
+        xxx_vertical = (board[0] == board[3] == board[6] and board[0] == "X") or (
+                        board[1] == board[4] == board[7] and board[1] == "X") or (
+                                       board[2] == board[5] == board[8] and board[2] == "X")
+        xxx_won = xxx_vertical or xxx_gorizontal or xxx_diagonal
 
+        ooo_gorizontal = (board[0] == board[1] == board[2] and board[0] == "O") or (
+                        board[3] == board[4] == board[5] and board[3] == "O") or (
+                                         board[6] == board[7] == board[8] and board[6] == "O")
+        ooo_diagonal = (board[0] == board[4] == board[8] and board[0]) == "O" or (
+                        board[2] == board[4] == board[6] and board[2] == "O")
+        ooo_vertical = (board[0] == board[3] == board[6] and board[0]) == "O" or (
+                        board[1] == board[4] == board[7] and board[1] == "O") or (
+                                       board[2] == board[5] == board[8] and board[2] == "O")
+        ooo_won = ooo_vertical or ooo_gorizontal or ooo_diagonal
+
+        there_ = board.__contains__(" ")
+
+        if (xxx_won and ooo_won):
+                print("Impossible")
+        else:
+                if xxx_won and not ooo_won:
+                        print("X wins")
+
+                elif ooo_won and not xxx_won:
+                        print("O wins")
+                elif not xxx_won and not ooo_won and not there_:
+                        print("Draw")
+                elif not xxx_won and not ooo_won and there_:
+                        if c == "X":
+                                c = "O"
+                                start(c)
+                        else:
+                                c = "X"
+                                start(c)
+
+def start(coor):
         coordinates = input().split(" ")
 
         a = int(coordinates[0]) - 1
         b = int(coordinates[1]) - 1
 
+
         if not coordinates[0].isalpha() or not coordinates[1].isalpha():
                 if (int(coordinates[0]) in range(0, 4) and int(coordinates[1]) in range(0, 4)):
-                        if updated_field[a][b] == "_":
-                                updated_field[a][b] = "X"
+                        if updated_field[a][b].isspace():
+                                updated_field[a][b] = coor
                                 print("---------")
                                 print("| " + " ".join(updated_field[0]) + " |")
                                 print("| " + " ".join(updated_field[1]) + " |")
                                 print("| " + " ".join(updated_field[2]) + " |")
                                 print("---------")
-                                exit()
+
+
+                                check_condition(updated_field, coor)
                         else:
                                 print("This cell is occupied! Choose another one!")
+                                start(coor)
                 else:
                         print("Coordinates should be from 1 to 3!")
+                        start(coor)
 
         else:
                 print("You should enter numbers!")
+                start(coor)
+start(shot)
 
 
 
 
-
-
-
-
-#
-# x = 0
-# o = 0
-# for i in "".join(field):
-#         if i == "X":
-#                 x += 1
-#         elif i == "O":
-#                 o += 1
-#
-#
-# xxx_gorizontal = (field[0] == field[1] == field[2] and field[0] == "X") or (field[3] == field[4] == field[5] and field[3] == "X") or (field[6] == field[7] == field[8] and field[6] == "X")
-# xxx_diagonal = (field[0] == field[4] == field[8] and field[0] == "X") or (field[2] == field[4] == field[6] and field[2] == "X")
-# xxx_vertical = (field[0] == field[3] == field[6] and field[0] == "X") or (field[1] == field[4] == field[7] and field[1] == "X") or (field[2] == field[5] == field[8] and field[2] == "X")
-# xxx_won = xxx_vertical or xxx_gorizontal or xxx_diagonal
-#
-# ooo_gorizontal = (field[0] == field[1] == field[2] and field[0] == "O") or (field[3] == field[4] == field[5] and field[3] == "O") or (field[6] == field[7] == field[8] and field[6] == "O")
-# ooo_diagonal = (field[0] == field[4] == field[8] and field[0]) == "O" or (field[2] == field[4] == field[6] and field[2] == "O")
-# ooo_vertical = (field[0] == field[3] == field[6] and field[0]) == "O" or (field[1] == field[4] == field[7] and field[1] == "O") or (field[2] == field[5] == field[8] and field[2] == "O")
-# ooo_won = ooo_vertical or ooo_gorizontal or ooo_diagonal
-#
-# there_ = field.__contains__("_")
-#
-#
-# if (xxx_won and ooo_won) or (x - o >= 2 or o - x >= 2):
-#         print("Impossible")
-# else:
-#         if xxx_won and not ooo_won:
-#                 print("X wins")
-#         elif ooo_won and not xxx_won:
-#                 print("O wins")
-#         elif not xxx_won and not ooo_won and there_:
-#                 print("Game not finished")
-#         elif not xxx_won and not ooo_won and not there_:
-#                 print("Draw")
 
 
 
